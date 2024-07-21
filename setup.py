d8e87bb78b8c2f818b0d938d30d3abb13d14c322
🔍 Data Scientist & Analyst <busra.ecem.sakar@gmail.com>
2024-07-21 12:01:08 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+def read_long_description():
+    try:
+        with open('README.md', 'r', encoding='utf-8') as f:
+            return f.read()
+    except FileNotFoundError:
+        return "A Python library for calculating distances between geographical coordinates."
+
+setup(
+    name='GeoDistanceTool',
+    version='0.1',
+    description='A Python library for calculating distances between geographical coordinates.',
+    long_description=read_long_description(),  # Include the long description from README
+    long_description_content_type='text/markdown',  # Set the format of the long description
+    author='Büşra Ecem Sakar',
+    author_email='busra.ecem.sakar@gmail.com',
+    url='https://github.com/BusraEcemSakar/GeoDistanceTool',  # URL to your GitHub repository
+    packages=find_packages(),
+    install_requires=[
+        'numpy',  # Example dependencies if needed
+    ],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='geographical distance haversine vincenty',
+    license='MIT',
+)
